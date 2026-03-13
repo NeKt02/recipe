368dd06a82b27ca4a44c47b847bd28e01acad81b
nnekkoo <ngorelov122@gmail.com>
2026-03-13 14:40:12 +0200

    new

--- script.js
+++ script.js
@@ -1,65 +1,82 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const API_KEY = "b587715be60e4e58a50b637037458d1e";
 
-    const API_KEY = "e7e39979cf9240378d334208a33ff014"
-
-    let cards_container = document.querySelector(".cards-container")
-    let recept_info = document.querySelector(".recept-info")
-
+    let cards_container = document.querySelector(".cards-container");
+    let recept_info = document.querySelector(".recept-info");
 
     async function fetchRecipes() {
+        const url = `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=2`;
 
-        const url = `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=2`
-        let response = await fetch(url)
-        let data = await response.json()
-        if (data.code == "402") {
-            alert("API key limit reached. Please try again later.")
-            return []
-        return data.recipes.length        
-        
+        let response = await fetch(url);
+        let data = await response.json();
+
+        if (data.code == 402) {
+            alert("API key limit reached. Please try again later.");
+            return [];
         }
-    
+
+        return data.recipes;
     }
 
-    data = fetchRecipes()
-
-
-
     async function translateText(safeText) {
-        let email = "bor.zdorovets28@gmail.com"
-        const url = `https://api.mymemory.translated.net/get?q=${safeText}&langpair=en|uk&de=${email}`;
-        let response = await fetch(url)
-        let data = await response.json()
-        return data.responseData.translatedText
-    } 
+        let email = "logikakyiv@gmail.com";
+        const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(safeText)}&langpair=en|uk&de=${email}`;
+        
+        let response = await fetch(url);
+        let data = await response.json();
+
+        return data.responseData.translatedText;
+    }
+
+    async function loadJson() {
+        let response = await fetch("data.json");
+        let data = await response.json();
+        return data.recipes;
+    }
+
+
 
     async function LoadCards() {
+        // let data = await fetchRecipes()
+
+        let data = await loadJson()
         console.log(data)
-        for (let i = 0; i < data; i++) {
-            let translatedsummary = await translateText(data[i].summary)
+        for (let i = 0; i < data.length; i++) {
+            data[i].translatedsummary = await translateText(data[i].summary)
             cards_container.innerHTML += `
             <div class="card">
                 <img src="${data[i].image}" alt="${data[i].title}">
                 <h2>${data[i].title}</h2>
-                <p>${translatedsummary.slice(0, 400)}...</p>
+                <p>${data[i].translatedsummary.slice(0, 400)}...</p>
                 <button class="button">Show more</button>
             </div>`
 
-            buuton = cards_container.querySelectorAll(".button")
-            for (let j = 0; j < buuton.length; j++) {
-                buuton[j].addEventListener("click", () => {
-                cards_container.classList.add("hide")
-                recept_info.classList.remove("hide")
-                recept_info.innerHTML = `<div class="recept-card">
-                    <img src="${data[i].image}" alt="${data[i].title}">
-                    <h2>${data[i].title}</h2>
-                    <p>${translatedsummary}</p>
-                    <button class="back-button">Back</button>
+            let button = cards_container.querySelectorAll(".button")
+            for (let j = 0; j < button.length; j++) {
+                button[j].addEventListener("click", () => {
+                    
+                    cards_container.classList.add("hide")
+                    recept_info.classList.remove("hide")
+                    recept_info.innerHTML = `<div class="recept-card">
+                        <img src="${data[j].image}" alt="${data[j].title}">
+                        <h2>${data[j].title}</h2>
+                        <p>${data[j].summary}</p>
+                        <button class="back-button">Back</button>
                     </div>`
+
+                    for (let k = 0; k < data[j].analyzedInstructions[0].steps.length; k++) {
+                        recept_info.querySelector(".recept-card").innerHTML += `<p>${data[j].analyzedInstructions[0].steps[k].step}</p>`
+                    }
+                    
+                    // Add event listener for back button
+                    recept_info.querySelector(".back-button").addEventListener("click", () => {
+                        recept_info.classList.add("hide")
+                        cards_container.classList.remove("hide")
+                    });
                 })
             }
         }
     }
 
 LoadCards()
-
 });
